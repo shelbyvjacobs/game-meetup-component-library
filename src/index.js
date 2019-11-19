cc313662978a6608d4758c5024bd25993b3d7733
Shelby Jacobs <shelbyvjacobs@gmail.com>
2019-11-19 15:25:54 -0500

    test

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import npm from "npm-demo-pkg";
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+exports.printMsg = function() {
+    console.log("exporting");
+  }
